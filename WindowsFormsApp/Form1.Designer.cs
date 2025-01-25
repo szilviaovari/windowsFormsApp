7e87c1955cb901da4b8dc01b5602a3c518e035a5
Szilvia <ovari.hugi804@gmail.com>
2025-01-25 17:14:38 +0100

    kinda database

--- WindowsFormsApp/Form1.Designer.cs
+++ WindowsFormsApp/Form1.Designer.cs
@@ -30,11 +30,39 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.nameUpdatebutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // helpProvider1
             // 
             this.helpProvider1.HelpNamespace = "C:\\Users\\Hugi\\Documents\\2025-2026\\project_windowsForms\\Sajat\\Ovari_Szilvia.html";
+            // 
+            // nameUpdatebutton
+            // 
+            this.nameUpdatebutton.BackColor = System.Drawing.Color.RosyBrown;
+            this.nameUpdatebutton.Font = new System.Drawing.Font("Modern No. 20", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameUpdatebutton.Location = new System.Drawing.Point(50, 41);
+            this.nameUpdatebutton.Margin = new System.Windows.Forms.Padding(0);
+            this.nameUpdatebutton.Name = "nameUpdatebutton";
+            this.nameUpdatebutton.Padding = new System.Windows.Forms.Padding(5);
+            this.nameUpdatebutton.Size = new System.Drawing.Size(121, 39);
+            this.nameUpdatebutton.TabIndex = 0;
+            this.nameUpdatebutton.Text = "Nevek frissítése";
+            this.nameUpdatebutton.UseVisualStyleBackColor = false;
+            this.nameUpdatebutton.Click += new System.EventHandler(this.nameUpdatebutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(349, 53);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(5);
+            this.label1.Size = new System.Drawing.Size(52, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nevek:";
             // 
             // Form1
             // 
@@ -43,6 +71,8 @@
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(606, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameUpdatebutton);
             this.HelpButton = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -52,12 +82,15 @@
             this.Text = "Hugi panel";
             this.HelpButtonClicked += new System.ComponentModel.CancelEventHandler(this.Form1_HelpButtonClicked);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.Button nameUpdatebutton;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/Form1.cs
@@ -4,9 +4,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
 
 namespace WindowsFormsApp
 {
@@ -20,6 +23,26 @@ namespace WindowsFormsApp
         private void Form1_HelpButtonClicked(object sender, CancelEventArgs e)
         {
             System.Diagnostics.Process.Start("C:\\Users\\Hugi\\Documents\\2025-2026\\project_windowsForms\\Sajat\\Ovari_Szilvia.html");
+        }
+
+        private void nameUpdatebutton_Click(object sender, EventArgs e)
+        {
+            string filePath = "nevek.txt";
+
+            if (File.Exists(filePath))
+            {
+                string[] names = File.ReadAllLines(filePath);
+
+                label1.Text = "Nevek:\n";
+                for (int i = 0; i < names.Length; i++)
+                {
+                    label1.Text += "\n" + names[i];
+                }
+            }
+            else
+            {
+                MessageBox.Show("A fájl nem található.");
+            }
         }
     }
 }
